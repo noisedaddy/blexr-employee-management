7cf9c2c6a95f0d24de37a2518d8343ec42796071
veljko <veljko@planetsg.com>
2020-11-02 10:00:42 -0500

    read me changed

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,5 +8,9 @@ npm install
 #clear cache from spatie package
 sudo php artisan cache:forget spatie.permission.cache && sudo php artisan cache:clear
 
+# change permissions of storage and bootstrap folders. Do not use in production
+sudo chmod -R 777 ../storage/
+sudo chmod -R 777 ../bootstrap/
+
 # Config and cache
 php artisan optimize:clear
